3e6d0256f4bba180eede6cdea557788ef1f80cf9
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-05 18:13:21 +0300

    function rain place changed to load cards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css'
-import { bigCard, getforcast } from './weather.js'
+import { bigCard, getforcast, slidercard } from './weather.js'
 const input = document.querySelector('[data-city-input]')
 const submitBtn = document.querySelector('[data-submit-location]')
-const drops = document.querySelectorAll('[data-drops]')
+const sliderCard = document.querySelector('[data-forecast')
 const cardHolder = document.querySelector('[data-cards')
+
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault()
   const cityName = input.value
@@ -11,17 +12,28 @@ submitBtn.addEventListener('click', (e) => {
   if (cityName === '') return
   const data = getforcast(cityName)
   data.then(response => {
+    console.log(1)
     cardHolder.innerHTML = bigCard(response)
+
+    console.log(2)
+
+    sliderCard.appendChild(slidercard(response, response.forecast.forecastday))
+    rain()
+    console.log(3)
   })
     .catch(err => {
       console.log(err)
     })
 })
-function random () {
-  const num = Math.floor(Math.random() * 3.5)
-  return num
-}
-console.log(drops)
-for (let i = 0; i < drops.length; i++) {
-  drops[i].style.animationDuration = `${random()}s`
+function rain () {
+  const drops = document.querySelectorAll('[data-drops]')
+
+  const random = () => {
+    const num = Math.floor(Math.random() * 3.5)
+    return num
+  }
+
+  for (let i = 0; i < drops.length; i++) {
+    drops[i].style.animationDuration = `${random()}s`
+  }
 }
