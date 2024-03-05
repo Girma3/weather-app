e9405e333ea45e67f92270074d21c3b0ad9c9c44
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-05 14:45:05 +0300

    function imported to show weather info

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-
 import './style.css'
-import { getforcast} from "./weather.js";
-const input = document.querySelector("[data-city-input]")
-const submitBtn = document.querySelector('[data-submit-location]');
-const  drops = document.querySelectorAll('[data-drops]')
-submitBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const cityName = input.value;
-    console.log(cityName)
-    if(cityName == "")return
-   console.log(getforcast('addis').currn)
-  
+import { bigCard, getforcast } from './weather.js'
+const input = document.querySelector('[data-city-input]')
+const submitBtn = document.querySelector('[data-submit-location]')
+const drops = document.querySelectorAll('[data-drops]')
+const cardHolder = document.querySelector('[data-cards')
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const cityName = input.value
 
+  if (cityName === '') return
+  const data = getforcast(cityName)
+  data.then(response => {
+    cardHolder.innerHTML = bigCard(response)
+  })
+    .catch(err => {
+      console.log(err)
+    })
 })
-function random(){
-    let num = Math.floor(Math.random() * 3.5)
-    return num
+function random () {
+  const num = Math.floor(Math.random() * 3.5)
+  return num
 }
-console.log(random())
-for(let i = 0;i < drops.length;i++){
-drops[i].style.animationDuration = `${random()}s`
+console.log(drops)
+for (let i = 0; i < drops.length; i++) {
+  drops[i].style.animationDuration = `${random()}s`
 }
-
