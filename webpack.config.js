85c0be572c71dd0b3797b678e2c0dd9b3c96bfa7
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-07 10:53:00 +0300

    favicon added

--- webpack.config.js
+++ webpack.config.js
@@ -8,35 +8,35 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'weather app',
-      template:"./src/template.html"
-    }),
+      favicon: './src/assets/images/favicon-32x32.png',
+      template: './src/template.html'
+    })
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    
-   
-   
+    clean: true
+
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.html$/,
         use: ['html-loader']
       }
-    ],
-  },
- 
+    ]
+  }
+
 };
