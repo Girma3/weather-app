2e13e8bd145b82eec9024b4ca5ccde23f2d14d57
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-07 10:34:32 +0300

    semi colon added and consol log statment removed

--- src/index.js
+++ src/index.js
@@ -1,52 +1,50 @@
 import './style.css'
 import { bigCard, focusStyle, getforcast, slidercard, validate, changeTemprature } from './weather.js'
-const input = document.querySelector('[data-city-input]')
-const submitBtn = document.querySelector('[data-submit-location]')
-const sliderCard = document.querySelector('[data-forecast')
-const cardHolder = document.querySelector('[data-cards')
-const errMsg = document.querySelector('[data-err-msg]')
-let shallow
+const input = document.querySelector('[data-city-input]');
+const submitBtn = document.querySelector('[data-submit-location]');
+const samllCard = document.querySelector('[data-forecast]');
+const cardHolder = document.querySelector('[data-cards');
+const errMsg = document.querySelector('[data-err-msg]');
+const loaderPage = document.querySelector('[data-loader-page]');
+let shallow;
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault()
-  let cityName = input.value
+  let cityName = input.value;
 
   if (validate(cityName, input, errMsg) === false) {
-    console.log('noppp')
+    return "can't find city name"
   } else if (validate(cityName, input, errMsg)) {
-    const data = getforcast(cityName)
+    const data = getforcast(cityName);
 
     data.then(response => {
-      cardHolder.innerHTML = bigCard(response)
-      sliderCard.textContent = ''
-      sliderCard.appendChild(slidercard(response, response.forecast.forecastday))
+      cardHolder.innerHTML = bigCard(response);
+      samllCard.textContent = '';
+      samllCard.appendChild(slidercard(response, response.forecast.forecastday))
       rain()
-      shallow = data
-
-      cityName = ''
-      input.value = ''
-      errMsg.textContent = ''
+      loaderPage.style.display = 'none';
+      // store data copy in shallow variable to use it later
+      shallow = data;
+      cityName = '';
+      input.value = '';
+      errMsg.textContent = '';
     })
       .catch(err => {
         errMsg.textContent = "can't find data,try another city"
-        console.log(err)
       })
   }
 })
 input.addEventListener('focus', () => {
   focusStyle(input, errMsg)
-  console.log(shallow)
 })
 function rain () {
-  const drops = document.querySelectorAll('[data-drops]')
-
+  const drops = document.querySelectorAll('[data-drops]');
   const random = () => {
     const num = Math.floor(Math.random() * 3.5)
     return num
   }
-
   for (let i = 0; i < drops.length; i++) {
-    drops[i].style.animationDuration = `${random()}s`
+    drops[i].style.animationDuration = `${random()}s`;
   }
 }
 const changeUnit = document.querySelector('[data-checkbox]')
@@ -57,11 +55,11 @@ changeUnit.addEventListener('click', () => {
 
   if (changeUnit.checked === true) {
     shallow.then(response => {
-      changeTemprature(temprature, firstTemprature, true, response)
+      changeTemprature(temprature, firstTemprature, true, response);
     })
   } else if (changeUnit.checked === false) {
     shallow.then(response => {
-      changeTemprature(temprature, firstTemprature, false, response)
+      changeTemprature(temprature, firstTemprature, false, response);
     })
   }
 })
